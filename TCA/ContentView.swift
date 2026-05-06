7f9f268e15f5a30d2665d47898e6f6dbd5bb439c
Влад Шимченко <vladshima99@gmail.com>
2026-05-06 11:27:19 +0300

    initial TCA

---
+++ TCA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCA
+//
+//  Created by Влад Шимченко on 6.05.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
